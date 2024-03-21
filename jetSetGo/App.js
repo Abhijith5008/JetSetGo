e7aa5e0432c7b40cef8b4bfba01b83ff581b1ca7
Abhijith5008 <ellikkal89@gmail.com>
2024-03-21 11:14:38 +0530

    Bug fix
    User getting logged out automatically .

--- jetSetGo/App.js
+++ jetSetGo/App.js
@@ -1,22 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Route from './Route';
 import { AuthContext } from './components/AuthContext';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const checkAuthentication = async () => {
-    const token = await AsyncStorage.getItem('token');
-    if (token) {
-      setIsLoggedIn(true);
-    }
-  };
-
-  useEffect(() => {
-    checkAuthentication();
-    AsyncStorage.removeItem('token');
-  }, []);
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
